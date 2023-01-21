1b75f2f2162d84a64c059cb662334ffbbba958d6
Filipe <joaofilipeb045@gmail.com>
2023-01-21 17:56:19 -0300

    Add lib dotenv

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,15 @@
+import "dotenv/config";
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+
+const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(3001, () => {
-    console.log("Server started on port 3001");
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
 });
