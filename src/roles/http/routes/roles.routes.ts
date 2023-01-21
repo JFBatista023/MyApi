7731452604f367be2a1610f498ac124f530e8f24
Filipe <joaofilipeb045@gmail.com>
2023-01-21 19:45:36 -0300

    Add roles repository

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,17 +1,13 @@
-import { Role } from "@roles/entities/Role";
+import { RolesRepository } from "@roles/repositories/RolesRepository";
 import { Router } from "express";
 
 const rolesRouter = Router();
-
-const roles: Role[] = [];
+const rolesRepository = new RolesRepository();
 
 rolesRouter.post("/", (request, response) => {
     const { name } = request.body;
-    const role = new Role();
+    const role = rolesRepository.create({ name });
 
-    Object.assign(role, { name, created_at: new Date() });
-
-    roles.push(role);
     return response.status(201).json(role);
 });
 
