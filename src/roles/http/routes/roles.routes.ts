1405b61bfdc51640520456ccaaf423ce4aec1447
Filipe <joaofilipeb045@gmail.com>
2023-01-21 20:00:40 -0300

    Create findByName method

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -6,8 +6,12 @@ const rolesRepository = new RolesRepository();
 
 rolesRouter.post("/", (request, response) => {
     const { name } = request.body;
-    const role = rolesRepository.create({ name });
+    const roleAlreadyExists = rolesRepository.findByName(name);
+    if (roleAlreadyExists) {
+        return response.status(400).json({ error: "Role already exists." });
+    }
 
+    const role = rolesRepository.create({ name });
     return response.status(201).json(role);
 });
 
