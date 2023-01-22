e986c332eb59c05eae43dc5fbff490146e5f4739
Filipe <joaofilipeb045@gmail.com>
2023-01-21 21:37:01 -0300

    Config database connection

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,40 +1,12 @@
 import "dotenv/config";
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
-import cors from "cors";
-import { routes } from "./routes";
-import { AppError } from "@shared/errors/AppError";
-import swaggerFile from "../../swagger.json";
+import "reflect-metadata";
+import { app } from "./app";
+import { dataSource } from "../typeorm";
 
 const PORT = process.env.PORT;
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(routes);
-
-app.use(
-    (
-        error: Error,
-        request: Request,
-        response: Response,
-        next: NextFunction,
-    ) => {
-        if (error instanceof AppError) {
-            return response
-                .status(error.statusCode)
-                .json({ status: "error", message: error.message });
-        }
-        console.log(error);
-        return response
-            .status(500)
-            .json({ status: "error", message: "Internal server error" });
-    },
-);
-
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+dataSource.initialize().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server started on http://localhost:${PORT}`);
+    });
 });
