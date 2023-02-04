34067eea11457b9ab00b5ee98173a4a21bbbf5fb
Filipe <joaofilipeb045@gmail.com>
2023-02-03 21:28:22 -0300

    Create migration CreateRefreshTokensTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,6 +4,7 @@ import { DataSource } from "typeorm";
 import { CreateRolesTable1674397560133 } from "./migrations/1674397560133-CreateRolesTable";
 import { CreateUsersTable1674862649254 } from "./migrations/1674862649254-CreateUsersTable";
 import { AddRoleIdToUsersTable1674863189664 } from "./migrations/1674863189664-AddRoleIdToUsersTable";
+import { CreateRefreshTokensTable1675470119684 } from "./migrations/1675470119684-CreateRefreshTokensTable";
 
 export const dataSource = new DataSource({
     type: "sqlite",
@@ -13,5 +14,6 @@ export const dataSource = new DataSource({
         CreateRolesTable1674397560133,
         CreateUsersTable1674862649254,
         AddRoleIdToUsersTable1674863189664,
+        CreateRefreshTokensTable1675470119684,
     ],
 });
